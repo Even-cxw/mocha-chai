69a20c020d8d9e7c10ef553948d15651d238744b
chenxuewei <chen.xuewei@yilutong.com>
2020-12-28 14:13:02 +0800

    测试钩子函数

--- test02/test/app.test.js
+++ test02/test/app.test.js
@@ -1,14 +1,15 @@
 // only skip 的使用
 var expact = require('chai').expect;
 var app = require('../app.js');
-describe('only skip', function() {
-  it.only('第一个', function() {
+describe('only skip测试', function() {
+  // it.only  it.skip
+  it('第一个', function() {
     expact(app.sumVal(1,1)).to.be.equal(2);
   })
-  it.skip('第二个', function() {
+  it('第二个', function() {
     expact(app.sumVal(1,2)).to.be.equal(3);
   })
-  it.only('第三个', function() {
+  it('第三个', function() {
     expact(app.sumVal(1,2)).to.be.equal(3);
   })
 })
--- test02/test/hook.test.js
+++ test02/test/hook.test.js
@@ -1,5 +1,33 @@
 var expact = require('chai').expect;
 var app = require('../app.js');
-describe('hook测试', function() {
+// 测试用例在被使用， 才会打印出日志
+describe('hook钩子函数', function() {
+  before(function() {
+    // 在本测试套件的“所有”测试用例之前执行
+    console.log('brfore的日志')
+  })
+
+  after(function() {
+    // 在被测试套件的“所有”测试用例之后执行
+    console.log('brfore的日志')
+  })
+
+  beforeEach(function() {
+    // 在本测试套件的“每个”测试用例之后执行
+    console.log('beforeEach的日志')
+  })
   
+  afterEach(function() {
+    // 在本测试套件的“每个”测试用例之后执行
+    console.log('afterEach的日志')
+  })
+
+  it('第一次', function() {
+    expact(app.sumVal(1,2)).to.be.equal(3);
+  })
+  it('第二次', function() {
+    expact(app.sumVal(1,1)).to.be.equal(2);
+  })
+
 })
+ 
