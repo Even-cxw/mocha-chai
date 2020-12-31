28d3bedab5d4669e143ce7873a69187210b98cc4
chenxuewei <chen.xuewei@yilutong.com>
2020-12-31 17:31:29 +0800

    解决test04报错问题，放弃es6的编译

--- test04/src/app.js
+++ test04/src/app.js
@@ -1,3 +1,6 @@
-export function add(a, b) {
+function add(a, b) {
   return a + b;
+}
+module.exports = {
+  add,
 }
--- test04/test/app.test.js
+++ test04/test/app.test.js
@@ -1,16 +1,15 @@
 // only skip 的使用
-// var expact = require('chai').expect;
-import {expact} from 'chai';
-import {add} from '../src/app.js';
+var expact = require('chai').expect;
+var app = require('../src/app.js');
 describe('only skip测试', function() {
   // it.only  it.skip
   it('第一个', function() {
-    expact(add(1,1)).to.be.equal(2);
+    expact(app.add(1,1)).to.be.equal(2);
   })
   it('第二个', function() {
-    expact(add(1,2)).to.be.equal(3);
+    expact(app.add(1,2)).to.be.equal(3);
   })
   it('第三个', function() {
-    expact(add(1,2)).to.be.equal(3);
+    expact(app.add(1,2)).to.be.equal(3);
   })
 })
